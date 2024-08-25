17d47a9c0c310c4fb93eda1b809b73848b4d5939
DanilaSysoev <danilasysoev@danilasysoev.ru>
2024-08-25 17:29:14 +0300

    Сделал рандомайзер на основе System.Random

---
+++ FeedEveryoneCore/Service/Randomizing/SystemRandomProvider.cs
@@ -0,0 +1,20 @@
+namespace FeedEveryone.Service.Randomizing;
+
+public class SystemRandomProvider : IRandomProvider
+{
+    public SystemRandomProvider()
+    {
+        random = new Random();
+    }
+    public SystemRandomProvider(int seed)
+    {
+        random = new Random(seed);
+    }
+
+    public float GetFloat()
+    {
+        return random.NextSingle();
+    }
+
+    private readonly Random random;
+}
