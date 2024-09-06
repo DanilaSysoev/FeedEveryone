3088120b97f83c4e75de1055393a3ddeb8b0b490
DanilaSysoev <danilasysoev@danilasysoev.ru>
2024-09-06 11:18:15 +0300

    Создал KeyboardControl для изоляции управления с клавы

---
+++ FeedEveryone.Mono/Control/KeyboardControl.cs
@@ -0,0 +1,14 @@
+using Microsoft.Xna.Framework.Input;
+
+namespace FeedEveryoneMono.Control;
+
+public static class KeyboardControl
+{
+    public static Keys CameraUp { get; set; } = Keys.W;
+    public static Keys CameraDown { get; set; } = Keys.S;
+    public static Keys CameraLeft { get; set; } = Keys.A;
+    public static Keys CameraRight { get; set; } = Keys.D;
+
+    public static Keys CameraZoomIn { get; set; } = Keys.Q;
+    public static Keys CameraZoomOut { get; set; } = Keys.E;
+}
