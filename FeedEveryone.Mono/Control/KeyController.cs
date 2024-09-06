b9d1ff6d4e8b224bc89314933e71a2e426c2dec9
DanilaSysoev <danilasysoev@danilasysoev.ru>
2024-09-06 11:17:43 +0300

    Создал KeyController для проверки разных ивентов клавиш

---
+++ FeedEveryone.Mono/Control/KeyController.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace FeedEveryoneMono.Control;
+
+public class KeyController
+{
+    public Keys Key { get; }
+    public float LongPressTime { get; set; } = DefaultLongPressTime;
+
+    public bool IsPressed => currentFrameState == KeyState.Down;
+    public bool IsReleased => currentFrameState == KeyState.Up;
+    public bool IsPressedOn => currentFrameState == KeyState.Down &&
+                               previousFrameState == KeyState.Up;
+    public bool IsReleasedOn => currentFrameState == KeyState.Up &&
+                                previousFrameState == KeyState.Down;
+    public bool IsLongPressed => IsPressed &&
+                                 LongPressTimeElapsed();
+
+    public event Action Pressed;
+    public event Action Released;
+    public event Action Press;
+    public event Action Release;
+    public event Action LongPresssed;
+
+    public KeyController(Keys key)
+    {
+        Key = key;
+    }
+
+    public void Update(GameTime gameTime)
+    {
+        _gameTime = gameTime;
+
+        previousFrameState = currentFrameState;
+        currentFrameState = Keyboard.GetState()[Key];
+
+        if (IsPressedOn) pressTime = gameTime.TotalGameTime;
+
+        InvokeEvents();
+    }
+
+    public const float DefaultLongPressTime = 1f;
+
+
+    private void InvokeEvents()
+    {
+        if (IsPressedOn) Press?.Invoke();
+        if (IsReleasedOn) Release?.Invoke();
+        if (IsPressed)
+        {
+            if (IsLongPressed && LongPresssed is not null)
+                LongPresssed.Invoke();
+            else
+                Pressed?.Invoke();
+        }
+        if (IsReleased) Released?.Invoke();
+    }
+
+    private bool LongPressTimeElapsed()
+    {
+        return (_gameTime.TotalGameTime - 
+                pressTime).TotalSeconds > LongPressTime;
+    }
+
+    private KeyState? previousFrameState = null;
+    private KeyState? currentFrameState;
+    private TimeSpan pressTime;
+    private GameTime _gameTime;
+}
